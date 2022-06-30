952d1b081b14387e80c05b8752363428f47c1859
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-07-01 00:03:17 +0530

    model and controller for post and get crud finished

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,9 +1,21 @@
 import postModel from "../models/postModel.js"
 
-export const getPost = (req, res)=> {
-    res.send("this works")
+export const getPost = async (req, res)=> {
+    try {
+        const postMessages = await postModel.find();
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
 }
 
-export const createPost = (req, res)=> {
-    res.send("Post create")
+export const createPost = async (req, res)=> {
+    const post = req.body;
+    const newPost = new postModel(post)
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
 }
